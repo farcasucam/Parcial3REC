acde0c9f3f3c3b77084393dff7b6da898e56caf1
[Frikismikis] <[adriansinlimites0912@gmail.com]>
2025-06-20 09:59:22 +0200

    Se han convertido los atributos de Alumno a privados, se han añadido getters y setters, y se ha cambiado ListaAlumnos para que funcione con dichos cambios.

--- Alumno.java
+++ Alumno.java
@@ -1,8 +1,8 @@
 public class Alumno {
-    public String nombre;
-    public double nota1;
-    public double nota2;
-    public double nota3;
+    private String nombre;
+    private double nota1;
+    private double nota2;
+    private double nota3;
 
     public Alumno(String n, double n1, double n2, double n3) {
         nombre = n;
@@ -13,5 +13,49 @@ public class Alumno {
 
     public double promedio() {
         return (nota1 + nota2 + nota3) / 3;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(double nota1) {
+        if (nota1 < 0 || nota1 > 10) {
+            throw new IllegalArgumentException("La nota debe estar entre 0 y 10");
+        } else {
+            this.nota1 = nota1;
+        }
+    }
+
+    public double getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(double nota2) {
+        if (nota2 < 0 || nota2 > 10) {
+            throw new IllegalArgumentException("La nota debe estar entre 0 y 10");
+        } else {
+            this.nota2 = nota2;
+        }
+    }
+
+    public double getNota3() {
+        return nota3;
+    }
+
+    public void setNota3(double nota3) {
+        if (nota3 < 0 || nota3 > 10) {
+            throw new IllegalArgumentException("La nota debe estar entre 0 y 10");
+        } else {
+            this.nota3 = nota3;
+        }
     }
 }
