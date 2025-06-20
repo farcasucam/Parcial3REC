2e44e171a25359c3bac5b963fae331bb4a375a77
[Frikismikis] <[adriansinlimites0912@gmail.com]>
2025-06-20 10:09:52 +0200

    Se ha creado la clase Asignatura, que cumple de manera mas eficiente las funciones de ListaAlumnos.

---
+++ Asignatura.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class Asignatura {
+    static ArrayList<Alumno> lista = new ArrayList<>();
+
+    public void mostrarAlumnos() {
+        System.out.println("Nombre\tNota1\tNota2\tNota3\tMedia");
+        System.out.println("--------------------------------------");
+        for (int i = 0; i < lista.size(); i++) {
+            Alumno a = lista.get(i);
+            System.out.println(a.getNombre() + "\t" +
+                String.format("%.1f", a.getNota1()) + "\t" +
+                String.format("%.1f", a.getNota2()) + "\t" +
+                String.format("%.1f", a.getNota3()) + "\t" +
+                String.format("%.1f", a.promedio()));
+        }
+    }
+
+    public double promedioGeneral() {
+        double suma = 0;
+        for (int i = 0; i < lista.size(); i++) {
+            suma += lista.get(i).promedio();
+        }
+        return suma / lista.size();
+    }
+
+    public Alumno darAltaAlumno(String nombre, double n1, double n2, double n3) {
+        Alumno a = new Alumno(nombre, n1, n2, n3);
+        return a;
+    }
+
+    public static void main(String[] args) {
+        lista.add(new Alumno("Ana", 8.5, 7.0, 9.0));
+        lista.add(new Alumno("Luis", 6.0, 7.5, 8.0));
+        lista.add(new Alumno("Carlos", 9.0, 8.5, 7.5));
+        lista.add(new Alumno("Maria", 7.0, 8.0, 8.5));
+        lista.add(new Alumno("Sofia", 8.0, 9.0, 7.0));
+    }
+}
