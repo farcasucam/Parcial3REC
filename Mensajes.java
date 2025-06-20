045e4044656fd801844bfd894edd24b45d3a6529
AGARCIA54-prog <agarcia868@alu.fpsanantonio.com>
2025-06-20 10:19:20 +0200

    Creada clase estatica y modificada la logica de las clases ListaAlumnos y Asignatura

---
+++ Mensajes.java
@@ -0,0 +1,7 @@
+public class Mensajes {
+    public static final String ENCABEZADO = "Nombre\tNota1\tNota2\tNota3\tMedia";
+    public static final String SEPARADOR = "--------------------------------------";
+    public static final String TITULO_LISTADO = "\tLISTADO DE ALUMNOS";
+    public static final String NOTA_MEDIA = "Nota media del grupo: ";
+    public static final String LIMPIAR_PANTALLA = "\033[H\033[2J";
+}
